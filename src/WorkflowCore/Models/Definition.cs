59d785578603a81e5fba84e6379f5316db41042c
StrakovVV <v.starckov2012@yandex.ru>
2024-01-28 12:14:32 +0400

    Добавлена модель определения

---
+++ src/WorkflowCore/Models/Definition.cs
@@ -0,0 +1,11 @@
+﻿namespace WorkflowCore.Models
+{
+    public class Definition
+    {
+        public string Id { get; set; }
+
+        public string Value { get; set; }
+
+        public string Version { get; set; }
+    }
+}
