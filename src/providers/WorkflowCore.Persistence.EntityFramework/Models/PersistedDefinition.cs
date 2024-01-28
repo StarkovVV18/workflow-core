d43bde73bc0547e61190a7f7a9e546f52eb76c29
StrakovVV <v.starckov2012@yandex.ru>
2024-01-28 12:14:59 +0400

    Добавлены модели для хранения в бд

---
+++ src/providers/WorkflowCore.Persistence.EntityFramework/Models/PersistedDefinition.cs
@@ -0,0 +1,11 @@
+﻿namespace WorkflowCore.Persistence.EntityFramework.Models
+{
+    public class PersistedDefinition
+    {
+        public string Id { get; set; }
+
+        public string Value { get; set; }
+
+        public string Version { get; set; }
+    }
+}
---
+++ src/providers/WorkflowCore.Persistence.EntityFramework/Models/PersistedTaskSchedule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WorkflowCore.Persistence.EntityFramework.Models
+{
+    public class PersistedTaskSchedule
+    {
+        public string Id { get; set; }
+
+        public string DefinitionId { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime CompleteTime { get; set; }
+
+        public bool IsProcessed { get; set; }
+
+        public string Result { get; set; }
+    }
+}
