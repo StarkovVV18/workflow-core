e93b126d6ef79429147b3bfb63b0cf7ef6e61e38
StrakovVV <v.starckov2012@yandex.ru>
2024-01-28 13:09:59 +0400

    Добавлен фоновый процесс запуска задач по расписанию

---
+++ src/WorkflowCore/Services/BackgroundTasks/RunnableTaskSchedule.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkflowCore.Interface;
+
+namespace WorkflowCore.Services.BackgroundTasks
+{
+    internal class RunnableTaskSchedule : IBackgroundTask
+    {
+        private readonly ILogger<RunnableTaskSchedule> _logger;
+        private readonly IPersistenceProvider _persistenceProvider;
+        private readonly IWorkflowController _workflowController;
+
+        public RunnableTaskSchedule(IPersistenceProvider persistenceProvider, IWorkflowController workflowController, ILogger<RunnableTaskSchedule> logger)
+        {
+            _persistenceProvider = persistenceProvider;
+            _workflowController = workflowController;
+            _logger = logger;
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
