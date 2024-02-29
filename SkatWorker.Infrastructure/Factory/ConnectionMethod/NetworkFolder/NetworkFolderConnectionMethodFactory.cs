446e48f733f04bbbc763887fbdf56bc1c1d3ffb8
StrakovVV <v.starckov2012@yandex.ru>
2024-02-29 16:04:07 +0400

    NetworkFolder способ подключения

---
+++ SkatWorker.Infrastructure/Factory/ConnectionMethod/NetworkFolder/NetworkFolderConnectionMethodFactory.cs
@@ -0,0 +1,12 @@
+﻿using SkatWorker.Application.Interfaces.Factory.ConnectionMethod;
+
+namespace SkatWorker.Infrastructure.Factory.ConnectionMethod.NetworkFolder
+{
+    internal class NetworkFolderConnectionMethodFactory : ConnectionMethodFactory
+    {
+        public override IConnectionTechnique GetConnectionTechnique()
+        {
+            return new NetworkFolderConnectionTechnique();
+        }
+    }
+}
---
+++ SkatWorker.Infrastructure/Factory/ConnectionMethod/NetworkFolder/NetworkFolderConnectionTechnique.cs
@@ -0,0 +1,22 @@
+﻿using SkatWorker.Application.Interfaces.Factory.ConnectionMethod;
+
+namespace SkatWorker.Infrastructure.Factory.ConnectionMethod.NetworkFolder
+{
+    public class NetworkFolderConnectionTechnique : ConnectionTechnique, IConnectionTechnique
+    {
+        public bool Connect(string url, string login, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Disconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] Download()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
