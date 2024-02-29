154f5f970d14ab2eb15baa1cd6a5e46655083c69
StrakovVV <v.starckov2012@yandex.ru>
2024-02-29 16:02:31 +0400

    Ftp способ подключения

---
+++ SkatWorker.Infrastructure/Factory/ConnectionMethod/Ftp/FtpConnectionMethodFactory.cs
@@ -0,0 +1,12 @@
+﻿using SkatWorker.Application.Interfaces.Factory.ConnectionMethod;
+
+namespace SkatWorker.Infrastructure.Factory.ConnectionMethod.Ftp
+{
+    public class FtpConnectionMethodFactory : ConnectionMethodFactory
+    {
+        public override IConnectionTechnique GetConnectionTechnique()
+        {
+            return new FtpConnectionTechnique();
+        }
+    }
+}
---
+++ SkatWorker.Infrastructure/Factory/ConnectionMethod/Ftp/FtpConnectionTechnique.cs
@@ -0,0 +1,22 @@
+﻿using SkatWorker.Application.Interfaces.Factory.ConnectionMethod;
+
+namespace SkatWorker.Infrastructure.Factory.ConnectionMethod.Ftp
+{
+    public class FtpConnectionTechnique : ConnectionTechnique, IConnectionTechnique
+    {
+        public bool Connect(string url, string login, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Disconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] Download()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
