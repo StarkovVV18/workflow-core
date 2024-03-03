74a1cc0759d811367ef50d354fc2ae5e4d649531
StrakovVV <v.starckov2012@yandex.ru>
2024-03-03 20:03:35 +0400

    Добавлен шаг для работы со службами Windows

---
+++ SkatWorker.Workflows/Workflows/Public/Steps/SystemService/SystemService.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Extensions.Logging;
+using SkatWorker.Infrastructure.Factory.ConnectionMethod.Http;
+using SkatWorker.Libraries.HttpClient.Builder;
+using WorkflowCore.Interface;
+using WorkflowCore.Models;
+using System.ServiceProcess;
+using SkatWorker.Workflows.Workflows.Public.Steps.SystemService;
+
+namespace SkatWorker.Workflows.Public.Steps.SystemService
+{
+    /// <summary>
+    /// Шаг для работы со службами Windows.
+    /// </summary>
+    public class SystemService : StepBody
+    {
+        /// <summary>
+        /// Логер.
+        /// </summary>
+        private readonly ILogger<SystemService> _logger;
+
+        /// <summary>
+        /// Имя сервиса.
+        /// </summary>
+        public string ServiceName { get; set; }
+
+        /// <summary>
+        /// Действие со службой.
+        /// </summary>
+        public SystemServiceCommand SystemServiceCommand { get; set; }
+
+        /// <summary>
+        /// Результат работы.
+        /// </summary>
+        public bool Result { get; set; }
+
+        /// <summary>
+        /// Результат состояния.
+        /// </summary>
+        public ServiceControllerStatus SystemServiceResult { get; set; }
+
+        public SystemService(ILogger<SystemService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override ExecutionResult Run(IStepExecutionContext context)
+        {
+            var services = ServiceController.GetServices();
+            var findedService = services.Where(x => x.ServiceName == ServiceName).FirstOrDefault();
+
+            if (findedService == null)
+                return ExecutionResult.Outcome($"Service {ServiceName} not found.");
+
+            if (SystemServiceCommand == SystemServiceCommand.Start)
+            {
+                findedService.Start();
+                SystemServiceResult = ServiceControllerStatus.StartPending;
+
+                return ExecutionResult.Next();
+            }
+
+            if (SystemServiceCommand == SystemServiceCommand.Start)
+            {
+                findedService.Stop();
+                SystemServiceResult = ServiceControllerStatus.StopPending;
+
+                return ExecutionResult.Next();
+            }
+
+            if (SystemServiceCommand == SystemServiceCommand.Status)
+            {
+                var statusService = findedService.Status;
+                SystemServiceResult = statusService;
+
+                return ExecutionResult.Next();
+            }
+
+            return ExecutionResult.Next();
+        }
+    }
+}
